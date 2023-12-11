b49ec5f7693d36d497d256c9948d31b75e825293
Nurul Affifah <113656648+Nurulaffifah11@users.noreply.github.com>
2023-12-11 19:26:09 +0700

    Add files via upload

---
+++ Segitiga/src/SegitigaSebelah.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class SegitigaSebelah {
+    public static void main (String[] args){
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Angka : ");
+        n = input.nextInt();
+
+        for (int i = 0; i < n * 2; i++) {
+            if (i < n) {
+                // Bagian pertama (pola menurun)
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+            } else if (i==n) {
+                continue;
+            } else {
+                // Bagian kedua (pola menaik)
+                for (int j = 0; j < n * 2 - i  ; j++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
